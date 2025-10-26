5c18bb00533a6c8e10402120f9aa01f040040625
Miyayu <38094366+yuchan2215@users.noreply.github.com>
2025-10-26 20:51:01 +0900

    Update teleport block label to include 'SUCCESS'

--- custom.ts
+++ custom.ts
@@ -5,7 +5,7 @@
 namespace hiragana_agent {
 
     //% blockId=hiragana_agent_8
-    //% block="agent teleport to player TEST8"
+    //% block="agent teleport to player TEST8 SUCCESS"
     export function teleportToPlayer(): void {
         agent.teleportToPlayer();
     }
